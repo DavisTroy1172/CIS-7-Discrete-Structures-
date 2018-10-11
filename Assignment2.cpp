c6e889a13b9bf3f9e97e591598bb6b06942920e9
Troy Davis <36419047+lakings859@users.noreply.github.com>
2018-10-10 20:25:04 -0700

    Create Assignment2.cpp

---
+++ Assignment2.cpp
@@ -0,0 +1,147 @@
+Troy Davis 
+Assignment2.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	char c;
+	int counter = 0;
+	string connectors = "!V^->";
+	string statements = "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz";
+	string input;
+	bool quit = false;
+	bool lastWasAlpha = false;
+	bool lastWasNot = false;
+	bool lastWasConnector = false;
+	bool isValidWff = true;
+	
+	
+
+	while (!quit)
+	{
+		counter = 0;
+
+		cout << "Enter a string: ";
+		
+		getline(cin, input);
+		cin.ignore();
+
+		for (unsigned int i = 0; i < input.length(); i++)
+		{
+
+			 c = input[i];
+			
+			if (c == ' ' || c == '-' || c == '(' || c == ')')
+			{
+				if (c == '(')
+				{
+					counter++;
+				
+					continue;
+					
+				}
+
+				else if (c == ')')
+				{
+					counter--;
+					
+					continue;
+				}
+
+				else
+				{
+					continue;
+				}
+			}
+			if (c == '!' || c == 'v' || c == '^' || c == '>')
+			{
+				if (c == '>' &&  i > 0)
+				{
+					if (input[i - 1] == '-')
+					{
+						lastWasConnector = true;
+						lastWasAlpha = false;
+						continue;
+					}
+					else
+					{
+						isValidWff = false;
+						break;
+					}
+
+				}
+
+				if (c == '!')
+				{
+					if (!lastWasAlpha)
+					{
+						isValidWff = false;
+						break;
+					}
+					lastWasConnector = false;
+
+				}
+				else
+				{
+					if (lastWasAlpha)
+					{
+						isValidWff = false;
+						break;
+					}
+					lastWasNot = true;
+
+				}
+				lastWasAlpha = false;
+			}
+			else if (isalpha(c))
+			{
+				if (lastWasAlpha)
+				{
+					isValidWff = false;
+					break;
+				}
+				lastWasAlpha = true;
+				lastWasConnector = false;
+				continue;
+			}
+			else
+			{
+				isValidWff = false;
+			}
+		}
+
+		if (counter != 0)
+		{
+			cout << " That is an invalid input of parentheses !\n";
+			isValidWff = false;
+		}
+		
+		
+
+		cout << (isValidWff ? "That is a WFF" : "NOT Valid WFF") << endl;
+		
+        cout << " Would you like to quit Y/N" << endl;
+		
+		char answer;
+		cin.get(answer);
+		cin.ignore();
+
+			if (toupper(answer) == 'Y')
+			{
+				quit = true;
+			}
+			else
+			{
+				quit = false;
+				
+			}
+			
+		
+		
+			
+	} 
+
+	return 0;
+}
